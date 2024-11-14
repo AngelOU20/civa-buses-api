5063f5b61a1348d7a925543eda7b9e3765510cef
Julio Ucharima <julio_ucharima@usmp.pe>
2024-11-14 18:54:28 -0500

    add BrandRequest DTO for brand data input

---
+++ src/main/java/com/civa/busesapi/model/dto/request/BrandRequest.java
@@ -0,0 +1,12 @@
+package com.civa.busesapi.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BrandRequest {
+    private String name;
+}
